51417d58840590a0d5decf50a1accaff8d5fce9c
Qasem <ziyaad.qasem@materna.de>
2021-01-16 14:50:07 +0100

    zz

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String home() {
-		return "Hello World! v1.1";
+		return "Hello World! v1.0";
 	}
 
 	public static void main(String[] args) {
